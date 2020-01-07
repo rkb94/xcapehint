598ba7af463fa394e7f38d7ab6b9af392d823dd6
Ryukyung Bin <rkbin@hayanmind.com>
2020-01-07 23:29:50 +0900

    - 알림창 'X' 버튼 '확인'으로 변경 - '501동 테마' 40분 진행 알람 35분으로 변경

--- public/js/main.js
+++ public/js/main.js
@@ -355,8 +355,8 @@ function startTimer(duration, display) { // 타이머...인데 일시정지 재�
             minutes = minutes < 10 ? "0" + minutes : minutes;
             seconds = seconds < 10 ? "0" + seconds : seconds;
             display.textContent = minutes + ":" + seconds;
-            if(display.id == "output1" && minutes + ":" + seconds == "40:00" && room1Flag){
-                displayBlockModal('크루님! 501동 사람들 테마가 40분 진행되었습니다.\n타임머신 가방 세팅을 완료하셨나요?');
+            if(display.id == "output1" && minutes + ":" + seconds == "35:00" && room1Flag){
+                displayBlockModal('크루님! 501동 사람들 테마가 35분 진행되었습니다.\n타임머신 가방 세팅을 완료하셨나요?');
                 room1Flag = false;
             }
         }
